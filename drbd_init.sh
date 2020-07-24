f5c9a488fe01148b164c2b87e165719e0c26b3d6
caojiaoyue <979477675@qq.com>
2020-07-24 16:33:48 +0800

    *

--- drbd_init.sh
+++ drbd_init.sh
@@ -5,6 +5,7 @@ drbdadm create-md pbxdata
 drbdadm up pbxdata
 ssh $1 "systemctl start drbd; sleep 1;drbdadm create-md pbxdata;drbdadm up pbxdata"
 ssh $2 "systemctl start drbd;sleep 1;drbdadm create-md pbxdata;drbdadm up pbxdata"
+sleep 10
 drbdadm -- --clear-bitmap new-current-uuid pbxdata
 drbdadm primary --force pbxdata
 mkfs.xfs /dev/drbd1
