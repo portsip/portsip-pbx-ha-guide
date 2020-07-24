f028200c1459678840ad3216055ad3c446c50140
caojiaoyue <979477675@qq.com>
2020-07-24 10:18:04 +0800

    添加drbd配置相关文件

--- drbd_init.sh
+++ drbd_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 drbdadm create-md pbxdata
 drbdadm up pbxdata
+ssh $1 "drbdadm create-md pbxdata;drbdadm up pbxdata"
+ssh $2 "drbdadm create-md pbxdata;drbdadm up pbxdata"
 drbdadm -- --clear-bitmap new-current-uuid pbxdata
 drbdadm primary --force pbxdata
 mkfs.xfs /dev/drbd1
 drbdadm secondary pbxdata
-ssh $1 "drbdadm create-md pbxdata;drbdadm up pbxdata"
-ssh $2 "drbdadm create-md pbxdata;drbdadm up pbxdata"
