d1e6957cc067e46b2c53ee76a71a61ee5932e852
caojiaoyue <979477675@qq.com>
2020-07-24 10:17:27 +0800

    修改drbd初始化

--- drbd_init.sh
+++ drbd_init.sh
@@ -5,5 +5,5 @@ drbdadm -- --clear-bitmap new-current-uuid pbxdata
 drbdadm primary --force pbxdata
 mkfs.xfs /dev/drbd1
 drbdadm secondary pbxdata
-ssh $1 "drbdadm create-md pbxdata"
-ssh $2 "drbdadm up pbxdata"
+ssh $1 "drbdadm create-md pbxdata;drbdadm up pbxdata"
+ssh $2 "drbdadm create-md pbxdata;drbdadm up pbxdata"
