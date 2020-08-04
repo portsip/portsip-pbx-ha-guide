6a0550534ab70230aa5b97b00d356de347ed1f3e
caojiaoyue <979477675@qq.com>
2020-08-04 11:49:50 +0800

    Modify code structure

--- install_pbx_docker_cn.sh
+++ install_pbx_docker_cn.sh
@@ -40,8 +40,9 @@ sudo apt-get remove -y  docker docker-engine docker.io containerd runc
 sudo apt update
 sudo apt upgrade
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce
 sudo systemctl enable docker
